775c5261990412b2c34f4e0bce4c1885b5fb06af
илья михайлов <ps.iria@yandex.ru>
2022-06-11 16:38:51 +0700

    Edit welkomer

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -7,4 +7,7 @@ public class Welcomer{
 	public String sayFarewell() {
 		return "Farewell, good hunter. May you find your worth in waking world.";
 	}
+    public String sayDream() {
+		return "A hunter is a hunter, even in a dream.";
+	}
 }
